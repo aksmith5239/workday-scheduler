18911cad6a3df5c057f6371475ff1b084cbc1a8d
aksmith5239 <aklobby@gmail.com>
2020-08-28 08:32:53 -0600

    js script added

---
+++ script.js
@@ -0,0 +1,13 @@
+//goal - create a schedule using a daily planner
+
+//open planner  - current day is displayed at top of calendar
+
+// scroll down to time blocks for standard business hours
+
+// view time blocks - each time block color coded to denote past, present future
+
+// click into time block and enter event
+
+// click save button to save event to local storage
+
+// 
